7ee7cefd7482f1dd92f0ab298c5e9c89e5d9f74f
rashmy <rashmy@RASHMY-PC2>
2020-03-19 22:27:34 -0700

    more changes

--- source/code/plugin/mdm_metrics_generator.rb
+++ source/code/plugin/mdm_metrics_generator.rb
@@ -2,24 +2,24 @@
 # frozen_string_literal: true
 
 class MdmMetricsGenerator
-    require "json"
-    require_relative "MdmAlertTemplates"
+  require "json"
+  require_relative "MdmAlertTemplates"
 
-    @@oom_killed_container_count_metric_name = 'oomKilledContainerCount'
+  @@oom_killed_container_count_metric_name = "oomKilledContainerCount"
 
-    def initialize
-    end
-  
-    class << self
-      def generatePodMetrics(metricName, podControllerName, podNamespace)
-        begin
-          errorMessage = "config::error::" + message
-          jsonMessage = errorMessage.to_json
-          STDERR.puts jsonMessage
-        rescue => errorStr
-          puts "Error in ConfigParserErrorLogger::logError: #{errorStr}"
-        end
+  def initialize
+    @oom_killed_container_count_hash = {}
+  end
+
+  class << self
+    def generatePodMetrics(metricName, podControllerName, podNamespace)
+      begin
+        # group by distinct dimension values
+        dim_key = [podControllerNameDimValue, podNamespaceDimValue].join("~~")
+        @oom_killed_container_count_hash[dim_key] = @oom_killed_container_count_hash.key?(dim_key) ? @oom_killed_container_count_hash[dim_key] + 1 : 1
+      rescue => errorStr
+        puts "Error in ConfigParserErrorLogger::logError: #{errorStr}"
       end
     end
   end
-  
+end
